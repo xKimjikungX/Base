bd5277b30bf07b5b8ea1386040ea0a441d33db0a
xKimjikungX <102714267+xKimjikungX@users.noreply.github.com>
2023-07-31 19:54:32 +0700

    Update RAMTool.lua

--- RAMTool.lua
+++ RAMTool.lua
@@ -43,8 +43,6 @@ repeat task.wait() until game:IsLoaded() and Nexus and not game:GetService("Play
 
 if not Nexus.IsConnected then Nexus.Connected:Wait() end
 
-Nexus.Commands.performance(7)
-
 local request = request or http_request or syn.request
 local TeleportService = game:GetService'TeleportService'
 
